d30b09638c9321329ea80b453cd5b5dee5403eaf
Rohit-Singh-0 <rohitsingh3640@gmail.com>
2026-03-11 14:35:09 +0530

    feat: Initialize Django project with Django Channels integration and base URL routing.

--- base/urls.py
+++ base/urls.py
@@ -14,7 +14,5 @@ urlpatterns = [
 
     path('get_token/', views.getToken),
 
-    path('create_member/', views.createMember),
     path('get_member/', views.getMember),
-    path('delete_member/', views.deleteMember),
 ]
--- mychat/asgi.py
+++ mychat/asgi.py
@@ -8,16 +8,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import base.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             base.routing.websocket_urlpatterns
